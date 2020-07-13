90e7a3335f8d537b65639ad1b6603769c25bd4d6
Safa <safasafari.ss@gmail.com>
2020-07-13 13:37:46 +0430

    BACK TO CURL
    fix warp+ bug

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
 $id = '3aff766b-8031-4674-aa70-8a9804ab2bd6';
-$data = json_encode([
-    'warp_enabled' => true,
-    'referrer' => $id,
-]);
-$header = array(
-    'Content-Type: application/json',
-    'User-Agent: okhttp/3.12.1',
-);
-$opts = array('http' =>
-array(
-    'method' => 'POST',
-    'header' => $header,
-    'content' => $data
-));
-$all = explode("\n", file_get_contents('proxy.txt'));
-foreach ($all as $t => $proxy) {
-    $opts['http']['proxy'] = 'tcp://' . trim($proxy);
-    $context  = stream_context_create($opts);
-    exec('title ' . $t . ' FROM ' . $sizeof($all));
-    for ($i = 0; $i < 2; $i++)
-        file_get_contents('https://api.cloudflareclient.com/v0a977/reg', false, $context);
+$proxytype = '';
+$e = explode("\n", file_get_contents($file));
+foreach ($e as $time => $proxy)
+	for ($i = 0; $i < 2; $i++)
+		connect('https://api.cloudflareclient.com/v0a977/reg', array('User-Agent: okhttp/3.12.1', 'Content-Type: application/json'), 'POST', array('referrer' => $id), $proxy, $proxytype);
+function connect($url, $header, $method = null, $data = null, $proxy = null, $proxytype = null)
+{
+	$ch = curl_init($url);
+	curl_setopt_array($ch, [
+		CURLOPT_HTTPHEADER => $header,
+		CURLOPT_SSLVERSION => CURL_SSLVERSION_MAX_TLSv1_1,
+		CURLOPT_USERAGENT => 'okhttp/3.12.1',
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_RETURNTRANSFER => true
+	]);
+	($method) ? curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method) : null;
+	($data) ? curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)) : null;
+	if ($proxy) {
+		$int = str_ireplace(['http', 'https', 'socks4', 'socks5', 'socks4a'], [0, 2, 4, 5, 6], $proxytype);
+		curl_setopt_array($ch, array(
+			CURLOPT_PROXYTYPE => $int,
+			CURLOPT_PROXY => $proxy
+		));
+	}
+	$res = curl_exec($ch);
+	curl_close($ch);
+	return $res;
 }
